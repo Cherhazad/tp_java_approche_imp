4e5c1409c30bcbbab89bf6db8af30fb76a1e6f22
Cherhazad <cherhazad.boustani@outlook.fr>
2024-04-30 22:28:10 +0200

    Exercice InteractifChiffresSuivants

---
+++ src/main/java/InteractifChiffresSuivants.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InteractifChiffresSuivants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Veuillez saisir un nombre: ");
+		int nb = scanner.nextInt();
+		for (int i=nb+1; i<=nb+10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
