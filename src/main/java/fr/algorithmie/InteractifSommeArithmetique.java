bdbd876d6b003aa39dd194d8ce8f5ca6d8dd9147
Cherhazad <cherhazad.boustani@outlook.fr>
2024-04-30 23:06:23 +0200

    Exercice InteractifSommeArithmetique

--- src/main/java/fr/algorithmie/InteractifSommeArithmetique.java
+++ src/main/java/fr/algorithmie/InteractifSommeArithmetique.java
@@ -1,9 +1,21 @@
 package fr.algorithmie;
 
+import java.util.Scanner;
+
 public class InteractifSommeArithmetique {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Veuillez saisir un nombre: ");
+		int nb = scanner.nextInt();
+		int sum = 0;
+		for (int i = 1; i <= nb; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
 
 	}
 
